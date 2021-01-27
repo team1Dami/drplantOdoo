5f91935a1e036f0eaac936b6085597041fc2001f
rubir <rubir@DESKTOP-4JP5PHE>
2021-01-27 20:10:50 +0100

    Modulo final

--- models/shop.py
+++ models/shop.py
@@ -1,10 +1,12 @@
 # To change this license header, choose License Headers in Project Properties.
 # To change this template file, choose Tools | Templates
 # and open the template in the editor.
+# Gonzalo
 
 from odoo import api
 from odoo import fields
 from odoo import models
+from odoo.exceptions import ValidationError
 
 class Shop(models.Model):
     _name = 'drplant.shop'
@@ -17,3 +19,9 @@ class Shop(models.Model):
 
     equipment = fields.One2many('drplant.equipment', 'id', string="Equipments")
     plant = fields.Many2many('drplant.plant',string="Plants")
+    
+    @api.constrains('commission')
+    def _check_commission(self):
+        for Shop in self:
+            if Shop.commission > 20:
+                raise ValidationError("The commission is too high: %s" % Shop.commission)
