69d924cdb588c426ef11ff46402b297330178261
rubir <rubir@DESKTOP-4JP5PHE>
2021-01-27 20:11:39 +0100

    Modulo final

--- models/plant.py
+++ models/plant.py
@@ -1,10 +1,12 @@
 # To change this license header, choose License Headers in Project Properties.
 # To change this template file, choose Tools | Templates
 # and open the template in the editor.
+# Ruben
 
 from odoo import api
 from odoo import fields
 from odoo import models
+from odoo.exceptions import ValidationError
 
 class Plant(models.Model):
     _name = 'drplant.plant'
@@ -19,6 +21,16 @@ class Plant(models.Model):
     climate = fields.Selection([('hot', 'hot'), ('cold', 'cold'), ('dry', 'dry'), ('wet', 'wet')], default='dry') 
     image = fields.Binary()
     
-    id_shop = fields.Many2many('drplant.shop', string="Tiendas")
-    id_plague = fields.Many2many('drplant.plague', string="Plagas")
+    id_shop = fields.Many2many('drplant.shop', string="Tiendas", ondelete="null")
+    id_plague = fields.Many2many('drplant.plague', string="Plagas", ondelete="null")
     id_userplant = fields.One2many('drplant.userplant', 'id_plant')
+    
+    
+    @api.constrains('watering_frequence')
+    def _check_watering_frequence_not_null(self):
+        for Plant in self:
+            if Plant.watering_frequence < 0:
+                raise ValidationError("You can`t enter a negative number: %s" % Plant.watering_frequence)
+            
+    
+    
